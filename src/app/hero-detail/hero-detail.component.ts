0e49433b3d8754da58475eb6f45f0e00f60522ec
tiaguinho-coder <tiagorncarvalho@gmail.com>
2020-03-15 22:53:54 +0000

    10 files modified, 5 added, 1 deleted

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -14,4 +14,9 @@ export class HeroDetailComponent implements OnInit {
   ngOnInit() {
   }
 
+  save(): void {
+   this.heroService.updateHero(this.hero)
+     .subscribe(() => this.goBack());
+ }
+
 }
