947ee5447ae3e12fcd1b5c2c47c5ccb01a58c083
tiaguinho-coder <tiagorncarvalho@gmail.com>
2020-03-18 15:07:26 +0000

    3 files modified

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -50,7 +50,7 @@ export class HeroDetailComponent implements OnInit {
   addPet(pet: Pet): void {
     this.hero.pet = pet;
     this.heroService.updateHero(this.hero).subscribe();
-    this.messageService.add(`HeroDetail: Added pet ${this.hero.pet}`);
+    this.messageService.add(`HeroDetail: Added pet ${this.hero.pet.name}`);
   }
 
  save(): void {
